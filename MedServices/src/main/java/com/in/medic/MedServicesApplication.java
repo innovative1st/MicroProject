14174bb653802d82015f6ebb7bb67c238522ba1d
Abhishek <connectabhi@mail>
2017-05-08 20:09:13 +0530

    Commited

---
+++ MedServices/src/main/java/com/in/medic/MedServicesApplication.java
@@ -0,0 +1,12 @@
+package com.in.medic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedServicesApplication.class, args);
+	}
+}
---
+++ MedServices/src/test/java/com/in/medic/MedServicesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.in.medic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MedServicesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
